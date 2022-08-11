67d0e7f4fc0b9a9392fc8668a361a738fe98ee8c
Ted Kulp <ted@tedkulp.com>
2022-08-11 17:21:47 -0400

    feat: search keymaps (taken from LunarVim)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -79,6 +79,20 @@ if wk_status_ok then
       c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
       C = { "<cmd>Telescope git_bcommits<cr>", "Checkout commit(for current file)" },
     },
+    ["<leader>s"] = {
+      name = "+Search",
+      b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+      c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+      f = { "<cmd>Telescope find_files<cr>", "Find File" },
+      h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+      H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+      M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+      r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+      R = { "<cmd>Telescope registers<cr>", "Registers" },
+      t = { "<cmd>Telescope live_grep<cr>", "Text" },
+      k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+      C = { "<cmd>Telescope commands<cr>", "Commands" },
+    },
     ["gr"] = { "Replace without yank" },
 
     -- LSP Keymaps -- removing conflicts - adding readable descriptions
