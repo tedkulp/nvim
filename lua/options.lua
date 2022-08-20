dc1b70291715205fefe76cbe3561beff66c01d6d
Ted Kulp <ted@tedkulp.com>
2022-08-20 15:55:13 -0400

    feat: neovide options (if that's your thing)

--- lua/options.lua
+++ lua/options.lua
@@ -30,6 +30,12 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
-vim.opt.shortmess:append "acsI"   -- disable nvim intro
+vim.opt.shortmess:append "acsI" -- disable nvim intro
 vim.opt.whichwrap:append "<>[]hl" -- go to previous/next line with h,l
 
+local _g = vim.g
+if _g['neovide'] then
+  _g['neovide_input_macos_alt_is_meta'] = true
+  _g['neovide_input_use_logo'] = true
+  _g['neovide_cursor_vfx_mode'] = "pixiedust"
+end
