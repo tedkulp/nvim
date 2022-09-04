5ba0fbda42818310bb0ee82db6e478aaf99ca392
Ted Kulp <ted@tedkulp.com>
2022-09-03 21:36:49 -0400

    fix: add more keys for iswap

--- lua/plugins/config/iswap.lua
+++ lua/plugins/config/iswap.lua
@@ -5,7 +5,7 @@ local wk_status_ok, wk = pcall(require, "which-key")
 if not wk_status_ok then return end
 
 iswap.setup({
-  keys = 'asdfjkl;qweruiop',
+  keys = 'asdfjkl;qweruiopghtyzxcvbnm',
 })
 
 wk.register({
