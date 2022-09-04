df4bdeeaf85ae88aefa1c32aefdaac635e125b6d
Ted Kulp <ted@tedkulp.com>
2022-09-03 21:40:05 -0400

    feat: trying out Octo -- not sure yet

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -294,6 +294,19 @@ return packer.startup(function(use)
     end,
   }
 
+  use {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = function()
+      require "octo".setup()
+    end
+  }
+
+
 
   ----------------------------
   -- Treesitter
