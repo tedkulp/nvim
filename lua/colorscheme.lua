061dfb33ecac50bf664cc195d77463557fcbd908
Ted Kulp <ted@tedkulp.com>
2022-11-11 08:05:42 -0500

    chore: show messages if we can't load color scheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -2,5 +2,6 @@ local colorscheme = "catppuccin"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
+  print("could not load color scheme:" .. colorscheme)
   return
 end
