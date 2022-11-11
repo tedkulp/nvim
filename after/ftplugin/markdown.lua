02b60b024ce81710e9c1c8dfacefff5b86960e28
Ted Kulp <ted@tedkulp.com>
2022-11-11 08:10:33 -0500

    fix: fixes for zk to link out of daily notes correctly

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,7 +1,10 @@
 vim.opt.conceallevel = 1
 
+Zk_status_ok, Zk_util = pcall(require, "zk.util")
+if not Zk_status_ok then return end
+
 local wk_status_ok, wk = pcall(require, "which-key")
-if wk_status_ok then
+if Zk_status_ok and wk_status_ok then
   wk.register({
     ["<leader>b"] = {
       m = {
@@ -12,14 +15,15 @@ if wk_status_ok then
   }, { mode = "n", buffer = 0 })
 
   -- Add the key mappings only for Markdown files in a zk notebook.
-  if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
+  if Zk_util.notebook_root(vim.fn.expand('%:p')) ~= nil then
     wk.register({
       ["Z"] = {
         ["n"] = { "<cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", "New Note",
           mode = "n" },
-        ["nt"] = { ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", "New Note w/ Title Selection",
+        ["nt"] = { ":'<,'>ZkNewFromTitleSelection { dir = Zk_util.notebook_root(vim.fn.expand('%:p:h')) }<CR>",
+          "New Note w/ Title Selection",
           mode = "v" },
-        ["nc"] = { ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
+        ["nc"] = { ":'<,'>ZkNewFromContentSelection { dir = Zk_util.notebook_root(vim.fn.expand('%:p:h')), title = vim.fn.input('Title: ') }<CR>",
           "New Note w/ Content Selection", mode = "v" },
         ["b"] = { "<cmd>ZkBacklinks<CR>", "Open backlinked notes", mode = "n" },
         ["l"] = { "<cmd>ZkLinks<CR>", "Open linked notes", mode = "n" },
